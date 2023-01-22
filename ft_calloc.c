daa1e7e663fb89756c0607860402d5872d12827a
tokuyama <okuyama0529work@gmail.com>
2023-01-23 00:56:04 +0900

    Upload ft_calloc.c for Libft-02

---
+++ ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okuyamatakahito <okuyamatakahito@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 19:34:15 by okuyamataka       #+#    #+#             */
+/*   Updated: 2023/01/23 00:21:48 by okuyamataka      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+	p = (void *)malloc(count*size));
+	if(*p != NULL)
+		ft_bzero(p, count * size)
+	return (NULL);
+}
