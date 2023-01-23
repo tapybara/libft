62c6b7cac703244d146b9f736c808a039bc41de7
tokuyama <okuyama0529work@gmail.com>
2023-01-23 23:31:29 +0900

    Upload ft_strdup.c & Fix some bugs of ft_calloc.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: okuyamatakahito <okuyamatakahito@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 01:44:28 by okuyamataka       #+#    #+#             */
-/*   Updated: 2023/01/22 19:35:45 by okuyamataka      ###   ########.fr       */
+/*   Updated: 2023/01/23 20:30:36 by okuyamataka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 
 void	*ft_calloc(size_t count, size_t size);
+char    *ft_strdup(const char *s);
 
 #endif
