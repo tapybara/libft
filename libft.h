cfdbf9ced8f0121804b6dea12efb541c34a75600
tokuyama <okuyama0529work@gmail.com>
2023-01-27 22:08:48 +0900

    Fix bugs for testers-KO

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: okuyamatakahito <okuyamatakahito@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 01:44:28 by okuyamataka       #+#    #+#             */
-/*   Updated: 2023/01/25 20:57:42 by okuyamataka      ###   ########.fr       */
+/*   Updated: 2023/01/27 18:57:23 by okuyamataka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,6 @@ void	*ft_calloc(size_t count, size_t size);
 char    *ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 #endif
